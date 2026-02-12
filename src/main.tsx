cdeb8ef6bd25860b29ab909bfe0b9fb45a06fc8f
thegrieferknight <grieferknight@yahoo.com>
2026-02-12 14:23:34 +0100

    feat: integrate tanstack query with devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from "./router";
+import { queryClient } from "./lib/query-client";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
